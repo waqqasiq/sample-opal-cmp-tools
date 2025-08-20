f4b04c532107ca59ab6cabf458af58aefd57b015
Waqqas Iqbal <waqqas.ess@gmail.com>
2025-08-20 15:54:29 +0600

    added client id and secret in config

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ dotenv.config();
 export const CMP_AUTH_URL = "https://accounts.cmp.optimizely.com/o/oauth2/v1/token";
 export const CMP_BASE_URL = "https://api.cmp.optimizely.com";
 
-export const CMP_CLIENT_ID = process.env.CLIENT_ID || "";
-export const CMP_CLIENT_SECRET = process.env.CLIENT_SECRET_DEV || "";
+export const CMP_CLIENT_ID = process.env.CLIENT_ID || "b3623097-4130-4c68-a727-6339cf305061";
+export const CMP_CLIENT_SECRET = process.env.CLIENT_SECRET_DEV || "789c72551e879d9a8d685083b8d0a40293e45a5d4529129dd65dfaee3caa2dda";
